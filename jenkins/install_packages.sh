0cb7f72f51b1543e2e07ebb14a48034535c71c9a
Shilpi Das <shilpi12@in.ibm.com>
2022-11-23 15:31:23 +0530

    Update install_packages according to jenkins

--- jenkins/install_packages.sh
+++ jenkins/install_packages.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# Installing ibmcloud cli
-curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
-
-ibmcloud plugin install container-service
-
 # Installing oc cli
 curl https://mirror.openshift.com/pub/openshift-v4/clients/oc/latest/linux/oc.tar.gz -o oc.tar.gz
 
@@ -13,7 +8,8 @@ tar xzf oc.tar.gz
 mv oc /usr/local/bin/
 mv kubectl /usr/local/bin/
 
-yum install unzip -y
+apt update
+apt install docker docker.io unzip
 
 curl https://dl.google.com/go/go1.18.linux-amd64.tar.gz -o go1.18.linux-amd64.tar.gz
 
